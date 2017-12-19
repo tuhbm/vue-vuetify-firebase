e14cca7a70da49794eb10fb1de15af888c5ea612
taekyunKim <tuhbm2@gmail.com>
2017-12-19 16:34:29 +0900

    vuetify 버전 최신화 & 타이틀 변경 & 도시 이미지 변경

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -36,8 +36,8 @@
       return {
         meetups: [
           { imageUrl: 'http://d1aueex22ha5si.cloudfront.net/TEMP/time-square-new-york-city-istock-487537456-2-1501663358549.jpg', id: 'fjdsklafjldksafjlkdsafj123', title: 'Meetup in NewYork' },
-          { imageUrl: 'https://media.cntraveler.com/photos/594bfa45d9f2557072bf37fe/master/w_775,c_limit/champs-elysees-GettyImages-601762977.jpg', id: 'fjdsklafjldksafjlkdsafj1234', title: 'Meetup in Paris' },
-          { imageUrl: 'http://www.travelweekly.com/uploadedImages/All_TW_Art/2016/110716/T1107SEOULATNIGHT_C.jpg?n=3266&width=750&height=420&scale=both&mode=crop', id: 'fjdsklafjldksafjlkdsafj112345', title: 'Meetup in Seoul' }
+          { imageUrl: 'http://europeanbusinessmagazine.com/wp-content/uploads/2017/07/paris.jpg', id: 'fjdsklafjldksafjlkdsafj1234', title: 'Meetup in Paris' },
+          { imageUrl: 'https://www.lonelyplanet.com/travel-blog/tip-article/wordpress_uploads/2016/09/shutterstock_407897917-lotte-world-tower-seoul-61bbde30f1cf.jpg', id: 'fjdsklafjldksafjlkdsafj112345', title: 'Meetup in Seoul' }
         ]
       }
     }
