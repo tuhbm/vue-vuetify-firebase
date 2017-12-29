d3fcd31d72611d8b593009e22231b9200d1ba6de
taekyunKim <tuhbm2@gmail.com>
2017-12-29 16:17:39 +0900

    로그인 및 로그아웃 추가

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,6 +1,87 @@
 <template>
-  <div>
-    <p>The Signup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex sx12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="onSignup">
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                <v-flex xs12>
+                  <v-text-field
+                    name="password"
+                    label="Password"
+                    id="password"
+                    v-model="password"
+                    type="password"
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    },
+    computed: {
+      comparePasswords () {
+        return this.password !== this.confirmPassword ? '비밀번호가 일치하지 않습니다.' : ''
+      },
+      user () {
+        return this.$store.getters.user
+      }
+    },
+    watch: {
+      user (value) {
+        if (value !== null && value !== undefined) {
+          this.$router.push('/')
+        }
+      }
+    },
+    methods: {
+      onSignup () {
+        this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
+      }
+    }
+  }
+</script>
 
